35e610204831a1c87cad3e97be56eaccda6cb8c5
viratvasu <36135781+viratvasu@users.noreply.github.com>
2021-04-24 14:19:44 +0530

    Update showProducts.js

--- src/showProducts.js
+++ src/showProducts.js
@@ -8,19 +8,21 @@ const ShowProducts = ({data,handleAddToCart})=>{
               <div className="image-container">
                 <img src={process.env.PUBLIC_URL+dataItem.src_1} alt={dataItem.title} />
               </div>
-              <p className="title">{dataItem.title}</p>
-              <div className="divider"></div>
-              <p>{dataItem.currencyFormat} {dataItem.price}</p>
-              <p style={{'color':'grey'}}>or {dataItem.installments} x {dataItem.currencyFormat} {(dataItem.price/dataItem.installments).toFixed(2)}</p>
-              <p>{dataItem.availableSizes.map(size=>size+",")}</p>
-              <button onClick={()=>handleAddToCart(dataItem.id)}>Add To cart</button>
+              <div className="box-body">
+                <p className="title">{dataItem.title}</p>
+                <div className="divider"></div>
+                <p>{dataItem.currencyFormat} {dataItem.price}</p>
+                <p style={{'color':'grey'}}>or {dataItem.installments} x {dataItem.currencyFormat} {(dataItem.price/dataItem.installments).toFixed(2)}</p>
+                {/* <p>{dataItem.availableSizes.map(size=>size+",")}</p> */}
+                <button onClick={()=>handleAddToCart(dataItem.id)}>Add To cart</button>
+              </div>
             </div>
           )
         })
       }
     return(
         <div className="products">
-            <center><p style={{'marginTop':'20px'}}>{data.length} product(s) found</p></center>
+            <p style={{'marginTop':'20px','textAlign':'center'}}>{data.length} product(s) found</p>
             <div className="box-container">{renderData()}</div>
         </div>
     )
