265ca0a955525a7b820c73f9e6ec6c6ba16757e1
viratvasu <36135781+viratvasu@users.noreply.github.com>
2021-04-24 14:19:13 +0530

    updated buttons with  map function

--- src/filters.js
+++ src/filters.js
@@ -1,17 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 const Filter = ({changeButtonStatus})=>{
+    const [data] = useState(['XS','S','M','ML','L','XL','XXL'])
     return(
         <div className="filter-container">
             <p style={{'textAlign':'center','fontSize':'1.2rem'}}>Sizes:</p>
             <div className="filters">
-            <button className="btn" onClick={e=>changeButtonStatus(e,"XS")}>XS</button>
-            <button className="btn" onClick={e=>changeButtonStatus(e,"S")}>S&nbsp;</button>
-            <button className="btn" onClick={e=>changeButtonStatus(e,"M")}>M</button>
-            <button className="btn" onClick={e=>changeButtonStatus(e,"ML")}>ML</button>
-            <button className="btn" onClick={e=>changeButtonStatus(e,"L")}>L&nbsp;</button>
-            <button className="btn" onClick={e=>changeButtonStatus(e,"XL")}>XL</button>
-            <button className="btn" onClick={e=>changeButtonStatus(e,"XXL")}>XXL</button>
-            <button className="btn active" onClick={e=>changeButtonStatus(e,"ALL")}>ALL</button>
+                {data.map(dataItem=><button className="btn" onClick={e=>changeButtonStatus(e,dataItem)}>{dataItem}</button>)}
+                <button className="btn active" onClick={e=>changeButtonStatus(e,"ALL")}>ALL</button>
             </div>
         </div>
     )
